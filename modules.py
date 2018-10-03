779fc9caa997fc3d4cc5d62906e9d733cdc9ff6e
mulondo <mulondo26@gmail.com>
2018-10-03 03:13:45 +0300

    delete

--- modules.py
+++
@@ -1,9 +0,0 @@
-class Bootcamper:
-    def __init__(self):
-        self.bootcampers=[]
-    def add_bootcamper(self,username,password):
-        bootcampr={"username":username,
-                    "password":password
-                    }
-        return self.bootcampers.append(bootcampr)
-    
